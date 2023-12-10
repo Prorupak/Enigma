bdeeb1a28efbf17effd537ad08bb0a2168338d8a
Rupak Sapota <rupaksapkota2000@gmail.com>
2023-12-10 10:46:39 +0545

    chore: Add entity constants and update index file

---
+++ backend/src/constants/entity.ts
@@ -0,0 +1,11 @@
+// membership roles
+export const ADMIN = "admin";
+export const MEMBER = "member";
+export const VIEWER = "viewer";
+export const CUSTOM = "custom";
+
+// membership statuses
+export const INVITED = "invited";
+
+// -- entity statuses
+export const ACCEPTED = "accepted";
